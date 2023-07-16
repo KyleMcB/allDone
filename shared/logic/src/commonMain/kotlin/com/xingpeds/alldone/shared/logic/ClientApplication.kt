76d3292095c14fa9a8f663d550a9235e30d18326
Kyle Mcburnett <kylemcburnett@xingpeds.com>
2023-07-16 08:08:00 -0700

    ClientApplication going well not fast

--- shared/logic/src/commonMain/kotlin/com/xingpeds/alldone/shared/logic/ClientApplication.kt
+++ shared/logic/src/commonMain/kotlin/com/xingpeds/alldone/shared/logic/ClientApplication.kt
@@ -4,13 +4,19 @@ import com.xingpeds.alldone.entities.*
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
-sealed class PairingState {
-    object Loading : PairingState()
-    object NotPaired : PairingState()
-    data class Paired(val serverAddress: Url) : PairingState()
-}
 
 interface ConnectionToServerFun : suspend (Url, CoroutineScope) -> AttemptedServerConnection
 
@@ -35,8 +41,52 @@ const val deviceKey = "deviceKey"
 class ClientApplication(
     val settings: PersistedSettings,
     val connectionToServer: ConnectionToServerFun,
-    val dispatcher: CoroutineDispatcher = Dispatchers.Default,
     val navigator: Navigator,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Default,
+    val appScope: CoroutineScope = CoroutineScope(dispatcher),
 ) {
+    private val device = MutableStateFlow<Device?>(null)
+    private val clientStateFlow = MutableStateFlow<ClientState>(ClientState.Loading)
+    val stateFlow: StateFlow<ClientState> = clientStateFlow
+
+    fun start(): Job = appScope.launch {
+        val stateString = settings.get(stateKey)
+        clientStateFlow.emit(stateString?.toClientState() ?: ClientState.TabulaRasa)
+        val deviceString = settings.get(deviceKey)
+        device.emit(deviceString?.toDeviceOrCreateNew() ?: Device(randUuid()))
+        clientStateFlow.onEach(::saveState).launchIn(appScope)
+        device.onEach(::saveDevice).launchIn(appScope)
+    }
+
+    private suspend fun saveState(clientState: ClientState) {
+        settings.set(stateKey, Json.encodeToString(clientState))
+    }
+
+    private suspend fun saveDevice(device: Device?) {
+        device ?: return
+        settings.set(deviceKey, Json.encodeToString(device))
+    }
+
+    suspend fun getDevice(): Device {
+        return device.filterNotNull().first()
+    }
+
+}
+
+@Serializable
+sealed class ClientState {
+    @Serializable
+    object Loading : ClientState()
+
+    @Serializable
+    object TabulaRasa : ClientState()
+}
+
+internal fun String.toClientState(): ClientState {
+    return Json.decodeFromString<ClientState>(this)
+}
+
+internal fun String.toDeviceOrCreateNew(): Device {
+    return Json.decodeFromString<Device>(this)
 }
 
--- shared/logic/src/commonTest/kotlin/ClientApplicationTestTDD.kt
+++ shared/logic/src/commonTest/kotlin/ClientApplicationTestTDD.kt
@@ -1,12 +1,16 @@
 import com.xingpeds.alldone.shared.logic.AttemptedServerConnection
 import com.xingpeds.alldone.shared.logic.ClientApplication
+import com.xingpeds.alldone.shared.logic.ClientState
 import com.xingpeds.alldone.shared.logic.ConnectionToServerFun
 import com.xingpeds.alldone.shared.logic.Navigator
 import com.xingpeds.alldone.shared.logic.PersistedSettings
 import com.xingpeds.alldone.shared.logic.Screen
 import com.xingpeds.alldone.shared.logic.Url
+import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
@@ -17,6 +21,17 @@ class TestNavigator : Navigator {
         events.emit(screen)
     }
 
+}
+
+class TestSettings(settings: Map<String, String> = mapOf()) : PersistedSettings {
+    private val map = settings.toMutableMap()
+    override suspend fun get(key: String): String? = map[key].also {
+        println("loaded $key = $it")
+    }
+
+    override suspend fun set(key: String, value: String) = map.set(key, value).also {
+        println("saved $key to $value")
+    }
 }
 
 class ClientApplicationTestTDD {
@@ -32,20 +47,33 @@ class ClientApplicationTestTDD {
         },
         navigator: Navigator = TestNavigator(),
     ) = ClientApplication(
-        settings = object : PersistedSettings {
-            val map = settings.toMutableMap()
-            override suspend fun get(key: String): String? = map[key]
-
-            override suspend fun set(key: String, value: String) = map.set(key, value)
-        },
+        settings = TestSettings(settings),
         connectionToServer = connectToServer,
         navigator = navigator,
+        appScope = this.backgroundScope,
     )
 
     @Test
     fun construct() = runTest {
         getTestSubject()
+        // if this test completes it is successful
     }
 
+    @Test
+    fun `device key is available after start`() = runTest {
+        val navigator = TestNavigator()
+        val subject = getTestSubject(navigator = navigator)
+        subject.start().join()
+        subject.getDevice()
+        //if this test completes it is successful
+    }
+
+    @Test
+    fun `app with no saved configuration starts in tabularasa state`() = runTest {
+        val navigator = TestNavigator()
+        val subject = getTestSubject(navigator = navigator)
+        subject.start().join()
+        subject.stateFlow.filterNotNull().first() shouldBe ClientState.TabulaRasa
+    }
 
 }
